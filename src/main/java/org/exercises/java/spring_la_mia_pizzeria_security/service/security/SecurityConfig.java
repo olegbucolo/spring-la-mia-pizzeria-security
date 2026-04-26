4e93ddaf646caf66a6aa45d542617c9f88823f22
olegbucolo <olegbucolo97@gmail.com>
2026-04-26 23:35:33 +0200

    Add authenticationProvider, passwordEncoder in SecurityConfig

--- src/main/java/org/exercises/java/spring_la_mia_pizzeria_security/service/security/SecurityConfig.java
+++ src/main/java/org/exercises/java/spring_la_mia_pizzeria_security/service/security/SecurityConfig.java
@@ -3,13 +3,34 @@ package org.exercises.java.spring_la_mia_pizzeria_security.service.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return provider;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
 
                 // /pizzas -> GET index USER, ADMIN --
@@ -43,4 +64,5 @@ public class SecurityConfig {
         return http.build();
 
     }
+
 }
